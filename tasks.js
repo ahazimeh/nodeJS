69af31fde9f44aff5628ca510799518b250899bf
ahazimeh <hazimeh95@gmail.com>
2020-10-19 09:58:26 +0300

    help command

--- tasks.js
+++ tasks.js
@@ -40,6 +40,9 @@ function onDataReceived(text) {
   else if(text === 'hello\n'){
     hello();
   }
+  else if(text === 'help\n'){
+    help();
+  }
   else{
     unknownCommand(text);
   }
@@ -76,6 +79,9 @@ function hello(){
 function quit(){
   console.log('Quitting now, goodbye!')
   process.exit();
+}
+function help(){
+  console.log('Type quit or exit to terminate\nType hello to print hello!')
 }
 
 
